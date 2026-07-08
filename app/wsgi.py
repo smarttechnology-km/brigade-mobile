93f45cb6fe92f98b1e73906813874421747673ed
smarttechnology-km <mohamed.a@smarttechnology-km.com>
2026-07-08 10:11:39 +0300

    Delete app/wsgi.py

--- app/wsgi.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""
-WSGI entry point for Gunicorn
-This file allows Gunicorn to properly import and run the Flask app
-"""
-from run import app
-
-if __name__ == "__main__":
-    app.run()
